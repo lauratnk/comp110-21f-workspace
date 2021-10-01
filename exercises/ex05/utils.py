e95db1c9706ea9f62c728d020438e9a50514e2f8
Laura Tinkler <lauratnk@live.unc.edu>
2021-09-30 20:36:02 -0400

    updates 9/30

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -3,3 +3,35 @@
 __author__ = "123456789"
 
 # Define your functions below
+
+
+def only_evens(ls1: list[int]) -> list[int]:
+    """Returns the even numbers in a list."""
+    i: int = 0
+    ls2: list[int] = []
+    while i < len(ls1):
+        if ls1[i] % 2 == 0:
+            ls2.append(ls1[i])
+        i += 1
+    return ls2
+
+
+def sub(ls1: list[int], start: int, end: int) -> list[int]:
+    """Returns a the numbers in a list within the given index."""
+    ls2: list[int] = []
+    # UHHHHHHHHH
+    return ls2
+
+
+def concat(ls1: list[int], ls2: list[int]) -> list[int]:
+    """Returns a list which contains all the elements of both lists."""
+    i: int = 0
+    ls3: list[int] = []
+    while i < len(ls1):
+        ls3.append(ls1[i])
+        i += 1
+    i2: int = 0
+    while i2 < len(ls2):
+        ls3.append(ls2[i2])
+        i2 += 1
+    return ls3
