c16a97c98befd1b4ead7c1508ec92ebc0eb98e62
Laura Tinkler <lauratnk@live.unc.edu>
2021-10-12 23:06:59 -0400

    updates 10/12

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -10,8 +10,8 @@ def invert(dict1: dict[str, str]) -> dict[str, str]:
     
     for x in dict1:
         """Iterates through each key in dict1."""
-        if dict1[x] in dict2 or x in dict2:
-            raise KeyError("Dictionary contains multiples of the same value.")
+        if dict1[x] in dict2:
+            raise KeyError("Input dictionary contains multiples of the same value.")
         else:
             dict2[dict1[x]] = x
 
@@ -21,27 +21,35 @@ def invert(dict1: dict[str, str]) -> dict[str, str]:
 def favorite_color(dict1: dict[str, str]) -> str:
     """Returns the value that appears the most frequently."""
     dict2 = dict()
-    
-    for x in dict1:
-        """For all keys in dict1, add key name to dict2, set key name = 0."""
-        dict2[x] = 0
 
+# For all values in dict1, add value name to dict2, set value name = 0.
     for x in dict1:
-        """Iterate through all keys in dict1. If key of dict2 in dict1 increase the value of key in dict2 by 1."""
-        if x in dict1:
-            dict2[x] += 1
+        dict2[dict1[x]] = 0
+
+# Iterate through all key[value] in dict1. If key of dict2 in dict1 increase the value of key in dict2 by 1.
+    for x in dict1:
+        if dict1[x] in dict2:
+            dict2[dict1[x]] += 1
 
     finalkeys: list = []
     finalvals: list = []
 
+# Separates dict2 into a list of final values and a list of final keys. 
     for x in dict2:
-        """Separates dict2 into a list of final values and a list of final keys."""
-        for x in dict2:
-            finalkeys.append(x)
-        for x in dict2:
-            finalvals.append(dict2[x])
+        finalkeys.append(x)
+    for x in dict2:
+        finalvals.append(dict2[x])
 
-    return finalkeys[finalvals[(max(finalvals))]]
+    c: str = "Error"
+    i: int = 0
+
+    while i < len(finalvals):
+        if finalvals[i] == max(finalvals):
+            z: int = i
+            return finalkeys[z]
+        i += 1
+
+    return c
 
 
 def count(ls1: list[str]) -> dict[str, int]:
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,13 +1,9 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
 __author__ = "730318786"
-
-
-from exercises.ex06.dictionaries import invert
-from exercises.ex06.dictionaries import count
-from exercises.ex06.dictionaries import favorite_color
 
 
 def test_invert1() -> None:
@@ -23,27 +19,9 @@ def test_invert2() -> None:
 
 
 def test_invert3() -> None:
-    """Raise keyerror."""
+    """Test invert."""
     a: dict[str, str] = {'kris': 'jordan', 'michael': 'jordan'}
     assert invert(a) == KeyError
-
-
-def test_count1() -> None:
-    """Test count."""
-    a = ['cat', 'dog', 'mouse', 'dog', 'dog', 'cat']
-    assert count(a) == {'cat': 2, 'dog': 3, 'mouse': 1}
-
-
-def test_count2() -> None:
-    """Test count."""
-    a = []
-    assert count(a) == {}
-
-
-def test_count3() -> None:
-    """Test count."""
-    a = ['cat', 'cat', 'cat']
-    assert count(a) == {'cat': 3}
 
 
 def test_favorite_color1() -> None:
@@ -62,3 +40,21 @@ def test_favorite_color3() -> None:
     """Tie for favorite."""
     a: dict[str, str] = {'laura': 'blue', 'kayla': 'blue', 'simone': 'green', 'sophie': 'green'}
     assert favorite_color(a) == 'blue'
+
+
+def test_count1() -> None:
+    """Test count."""
+    a: list[str] = ['cat', 'dog', 'mouse', 'dog', 'dog', 'cat']
+    assert count(a) == {'cat': 2, 'dog': 3, 'mouse': 1}
+
+
+def test_count2() -> None:
+    """Test count."""
+    a = []
+    assert count(a) == {}
+
+
+def test_count3() -> None:
+    """Test count."""
+    a = ['cat', 'cat', 'cat']
+    assert count(a) == {'cat': 3}
